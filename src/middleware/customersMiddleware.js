e48f9579b28308c018e311b7bd07a234eecd41c9
Pedro Pereira <plp.leoncio@gmail.com>
2022-10-03 16:33:43 -0300

    .

--- src/middleware/customersMiddleware.js
+++ src/middleware/customersMiddleware.js
@@ -1,11 +1,29 @@
 import connection from '../database/database.js';
+import customersSCHEMA from '../schemas/customersSchema.js'
 //import schema
 
 async function customersGetMiddleware (req, res, next){
     next()
 }
 async function customersPostMiddleware (req, res, next){
-    next()
+
+    const isValid = customersSCHEMA.validate(req.body, {abortEarlt: false})
+
+    if (isValid.error){
+        return res.status(400).send(isValid.error.message)
+    }
+    try {
+        const hasUser = await connection.query(`SELECT * FROM customers WHERE cpf=$1`, [req.body.cpf])
+        if(hasUser.rows.length !== 0){
+            return res.sendStatus(409)
+        }
+        else {
+            next()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 async function customersPutMiddleware (req, res, next){
     next()
---
+++ src/schemas/customersSchema.js
@@ -0,0 +1,10 @@
+import joi from 'joi'
+
+const categoriesSCHEMA = joi.object({
+    name:joi.required(),
+    cpf: joi.string().min(11).max(11).required(),
+    phone: joi.string().min(10).max(11).required(),
+    birthday: joi.date().format('YYYY-MM-DD'),
+})
+
+export {categoriesSCHEMA}
